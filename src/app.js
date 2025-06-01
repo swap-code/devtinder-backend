a797495b66525a610115eb9d466a45156f5422c2
yadavswapnil309@gmail.com <yadavswapnil309@gmail.com>
2025-06-01 21:26:02 +0530

    middleware use for authentication

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const express= require('express');
 const app= express();
 
-app.use("/test",(req,res)=>{
-    res.send("test is work")
+// Middleware Use
+app.use("/user",(req,res,next)=>{
+    const tocken= "xyz";
+    const isAuth=tocken=== "xyz";
+    if(!isAuth){
+        return resd.status(401).json({message:"Unauthorized"});
+    }
+    else{
+        next()
+    }
 })
 
-app.use("/test2",(req,res)=>{
-    res.send("test2 is work")
-});
+app.get("/user/getAllData",(req,res)=>{
+    res.status(200).send("All user data fetched successfully");
+})
+app.delete("/user/deleteData",(req,res)=>{
+    res.status(200).send("User data deleted successfully");
+})
+app.post("/user/createData",(req,res)=>{
+    res.status(201).send("User data created successfully");
+})  
 
-app.use("/test3",(req,res)=>{
-    res.send("test3 is work")
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
 });
-app.listen(3000);
